2b9cda1db195af9a010f0bb35faed6b66fc0a7ec
Carl Poelking <carl.poelking@mpip-mainz.mpg.de>
2012-01-24 14:01:56 +0100

    CtpMap -> Trajectoryp parsing

--- src/tools/ctp_map_exp.cc
+++ src/tools/ctp_map_exp.cc
@@ -70,21 +70,95 @@ bool CtpMapExp::EvaluateOptions() {
 
 void CtpMapExp::Run() {
 
-    string topfile = _op_vm["top"].as<string> ();
-    TopologyReader *reader;
-    reader = TopReaderFactory().Create(topfile);
+    // ++++++++++++++++++++++++++++ //
+    // Create MD topology from file //
+    // ++++++++++++++++++++++++++++ //
 
-    if (reader == NULL) {
-        throw runtime_error( string("Input format not supporeted: ")
+    // Create topology reader
+    string topfile = _op_vm["top"].as<string> ();
+    TopologyReader *topread;
+    topread = TopReaderFactory().Create(topfile);
+
+    if (topread == NULL) {
+        throw runtime_error( string("Input format not supported: ")
                            + _op_vm["top"].as<string> () );
     }
 
-    reader->ReadTopology(topfile, this->_mdtopol);
+    topread->ReadTopology(topfile, this->_mdtopol);
     cout << "MD Topology from " << topfile << ": Found "
          << _mdtopol.BeadCount() << " atoms in "
          << _mdtopol.MoleculeCount() << " molecules. "
          << endl;
-    
+
+    // ++++++++++++++++++++++++++++++ //
+    // Create MD trajectory from file //
+    // ++++++++++++++++++++++++++++++ //
+
+    // Create trajectory reader and initialize
+    string trjfile =  _op_vm["trj"].as<string> ();
+    TrajectoryReader *trjread;
+    trjread = TrjReaderFactory().Create(trjfile);
+
+    if (trjread == NULL) {
+        throw runtime_error( string("Input format not supproated: ")
+                           + _op_vm["trj"].as<string> () );
+    }
+    trjread->Open(trjfile);
+    trjread->FirstFrame(this->_mdtopol);
+
+    int    firstFrame = 1;
+    int    frameNo    = 0;
+    bool   beginAt    = 0;
+    double startTime  = _mdtopol.getTime();
+
+    if (_op_vm.count("nframes")) {
+        frameNo = _op_vm["nframes"].as<int> ();
+    }
+    if (_op_vm.count("first-frame")) {
+        firstFrame = _op_vm["first-frame"].as<int> ();
+    }    
+    if (_op_vm.count("begin")) {
+        beginAt = true;
+        startTime = _op_vm["begin"].as<double> ();
+    }
+
+    // Extract first frame specified
+    bool hasFrame;
+
+    for (hasFrame = true; hasFrame == true;
+         hasFrame = trjread->NextFrame(this->_mdtopol)) {
+         if (  ((_mdtopol.getTime() < startTime) && beginAt )
+               || firstFrame > 1 ) {
+             firstFrame--;
+             continue;
+         }
+         break;
+    }
+    if ( ! hasFrame) {
+        trjread->Close();
+        delete trjread;
+
+        throw runtime_error("Time or frame number exceeds trajectory length");
+    }
+    /*
+    MoleculeContainer::iterator mit;
+    for (mit = _mdtopol.Molecules().begin();
+         mit != _mdtopol.Molecules().end();
+         mit++ ) {
+
+        Molecule *mol = *mit;
+        for (int i = 0; i < mol->BeadCount(); i++) {
+            Bead* btm = mol->getBead(i);
+            cout << "ATOM getId  " << btm->getId();
+            cout << " | getName  " << btm->getName();
+            cout << " | getMol   " << btm->getMolecule()->getId();
+            cout << " | MolName  " << btm->getMolecule()->getName();
+            cout << " | getResnr " << btm->getResnr();
+            cout << " | getType  " << btm->getType()->getName();
+            cout << " | getPos   " << btm->getPos();
+            cout << endl;
+        }    
+    } */
 }
 
 
@@ -92,4 +166,4 @@ int main(int argc, char** argv)
 {
     CtpMapExp ctpmap;
     return ctpmap.Exec(argc, argv);
- }
+}
